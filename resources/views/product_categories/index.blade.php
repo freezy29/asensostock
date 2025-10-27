d7928acaea530bceca26f904fae2497b85a94c9c
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2025-10-27 15:22:57 +0800

    categories index

--- resources/views/product_categories/index.blade.php
+++ resources/views/product_categories/index.blade.php
@@ -1,82 +1,95 @@
 <x-layouts.app>
   <x-slot:title>Categories</x-slot:title>
 
-    <div class="flex flex-col md:flex-row w-full md:justify-between md:items-end">
-        <div>
-            <x-ui.breadcrumbs>
-                <li><a>Dashboard</a></li>
-                <li>Categories</li>
-            </x-ui.breadcrumbs>
-            <h1 class="text-4xl font-bold mb-2">Categories</h1>
-        </div>
+            <x-partials.header>
 
-        <div>
-            <a href="{{ route('categories.create') }}" class="btn">add category</a>
-        </div>
-    </div>
+                <x-slot:breadcrumb_list>
+                    <li>Categories</li>
+                </x-slot:breadcrumb_list>
 
-    <div class="divider"></div>
+                <x-slot:page_title>
+                  Categories
+                </x-slot:page_title>
+
+
+            <label class="input">
+                <svg class="h-[1em] opacity-50" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <g
+                        stroke-linejoin="round"
+                        stroke-linecap="round"
+                        stroke-width="2.5"
+                        fill="none"
+                        stroke="currentColor"
+                    >
+                        <circle cx="11" cy="11" r="8"></circle>
+                        <path d="m21 21-4.3-4.3"></path>
+                    </g>
+                </svg>
+                <input type="search" required placeholder="Search" />
+            </label>
+
+            @can('create', App\Models\ProductCategory::class)
+            <x-ui.buttons.create href="{{ route('categories.create') }}">
+                Add Category
+            </x-ui.buttons.create>
+            @endcan
+
+        </x-partials.header>
 
     <div class="overflow-x-auto m-8">
         <table class="table table-zebra table-lg">
             <!-- head -->
             <thead>
                 <tr>
-                    <th>ID</th>
-                    <th>Name</th>
+                    <th></th>
+                    <th>Category Name</th>
+                    @if (auth()->user()->role === 'admin')
                     <th>Status</th>
+                    @endif
                     <th>Actions</th>
                 </tr>
             </thead>
             <tbody>
-              @foreach ($categories as $category)
+              @forelse ($categories as $category)
                 <tr>
-                    <td>{{ $category->id }}</td>
+                    <td></td>
                     <td>{{ $category->name }}</td>
-                    <td>{{ $category->status }}</td>
+
+                    @if (auth()->user()->role === 'admin')
                     <td>
 
-                    <div class="tooltip" data-tip="View Details">
-                        <a href="{{ route('categories.show', $category->id) }}" class="btn btn-square">
-                          <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-gray-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.477 0 8.268 2.943 9.542 7-1.274 4.057-5.065 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-                          </svg>
-                        </a>
-                    </div>
+                    @if(strtolower($category->status) === 'active')
+                      <span class="badge badge-success badge-md">Active</span>
+                    @else
+                      <span class="badge badge-error badge-md">Inactive</span>
+                    @endif
 
+                    </td>
+                    @endif
+                    <td>
 
-                    <div class="tooltip" data-tip="Edit">
-                        <a href="{{ route('categories.edit', $category->id) }}" class="btn btn-square">
-                          <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-yellow-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M15.232 5.232l3.536 3.536" />
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M4 20l4-1 11-11-3-3L6 16l-2 4z" />
-                          </svg>
-                        </a>
-                    </div>
+                        <x-ui.buttons.view href="{{ route('categories.show', $category->id) }}">
+                        </x-ui.buttons.view>
 
-                        <div class="tooltip" data-tip="Delete">
-                        <form method="POST" action="{{ route('categories.destroy', $category->id) }}">
-                            @csrf
-                            @method('DELETE')
-                            <button
-                                type="submit"
-                                onclick="return confirm('Are you sure you want to delete this user?')"
-                                class="btn btn-square">
-                              <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-red-600" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="1.5">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M6 7h12" />
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M10 11v6m4-6v6" />
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M9 7l1-3h4l1 3" />
-                              </svg>
-                            </button>
-                            </form>
-                        </div>
+                    @canany(['update', 'delete'], $category)
+
+                        <x-ui.buttons.edit href="{{ route('categories.edit', $category->id) }}">
+                        </x-ui.buttons.edit>
+
+                        <x-ui.buttons.delete action="{{ route('categories.destroy', $category->id) }}">
+                        </x-ui.buttons.delete>
+                    @endcanany
 
                     </td>
                 </tr>
-                @endforeach
+                  @empty
+                <tr>
+                  <td colspan="4" class="text-center text-gray-500 py-6">No categories yet.</td>
+                </tr>
+                @endforelse
             </tbody>
         </table>
     </div>
+
 
 </x-layouts.app>
