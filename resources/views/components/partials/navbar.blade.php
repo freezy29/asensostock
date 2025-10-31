5a9977511d725635f5a0dfdb857e2c7a278e3173
ttrriipp <136776800+ttrriipp@users.noreply.github.com>
2025-10-31 15:58:25 +0800

    avatar placeholder

--- resources/views/components/partials/navbar.blade.php
+++ resources/views/components/partials/navbar.blade.php
@@ -61,8 +61,7 @@
             <div class="w-10 rounded-full">
 
               <img
-                alt="Tailwind CSS Navbar component"
-                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
+                src="https://ui-avatars.com/api/?name={{ urlencode(auth()->user()->first_name . " " . auth()->user()->last_name) }}&background=random" />
             </div>
         </div>
      @auth
